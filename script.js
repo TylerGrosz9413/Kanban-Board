09b143e59face5161ccb374b13c64df2a646ea73
Tyler Grosz <tyler-grosz@outlook.com>
2023-04-06 10:23:04 -0500

    refactored code

--- script.js
+++ script.js
@@ -140,14 +140,14 @@ function showInputBox(column) {
     addItemContainers[column].style.display = 'flex';
 }
 
-//Hide item input box
+// Hide item input box
 function hideInputBox(column) {
     addBtns[column].style.visibility = 'visible';
     saveItemBtns[column].style.display = 'none';
     addItemContainers[column].style.display = 'none';
     addToColumn(column);
 }
-// Allow arrays to  reflect drag and drop items
+// Allow arrays to reflect drag and drop items
 function rebuildArrays() {
 
     backlogListArray = Array.from(backlogList.children).map(i => i.textContent);
@@ -155,22 +155,6 @@ function rebuildArrays() {
     completeListArray = Array.from(completeList.children).map(i => i.textContent);
     onHoldListArray = Array.from(onHoldList.children).map(i => i.textContent);
 
-    // backlogListArray = [];
-    // for (let i = 0; i < backlogList.children.length; i++) {
-    //     backlogListArray.push(backlogList.children[i].textContent);
-    // }
-    // progressListArray = [];
-    // for (let i = 0; i < progressList.children.length; i++) {
-    //     progressListArray.push(progressList.children[i].textContent);
-    // }
-    // completeListArray = [];
-    // for (let i = 0; i < completeList.children.length; i++) {
-    //     completeListArray.push(completeList.children[i].textContent);
-    // }
-    // onHoldListArray= [];
-    // for (let i = 0; i < onHoldList.children.length; i++) {
-    //     onHoldListArray.push(onHoldList.children[i].textContent);
-    // }
     updateDOM();
 }
 
@@ -185,7 +169,7 @@ function allowDrop(e) {
     e.preventDefault();
 }
 
-//When item enters column area
+// When item enters column area
 function dragEnter(column) {
 listColumns[column].classList.add('over');
 currentColumn = column;
@@ -202,7 +186,7 @@ function drop(e) {
     const parent = listColumns[currentColumn];
     parent.appendChild(draggedItem);
 
-    //Dragging complete
+    // Dragging complete
     dragging = false;
     rebuildArrays();
 }
